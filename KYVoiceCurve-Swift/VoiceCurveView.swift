a0e3803e3adcd4d938a1d12a9813a39afb0f7af7
KittenYang <kittenyang@icloud.com>
2015-04-09 00:23:06 +0800

    updated

--- KYVoiceCurve-Swift/VoiceCurveView.swift
+++ KYVoiceCurve-Swift/VoiceCurveView.swift
@@ -55,12 +55,11 @@ class VoiceCurveView: UIView {
         self.setUp()
         sv.addSubview(self)
     }
-    
-    
+
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-        super.init(coder: aDecoder)
     }
+    
 
     func setUp() ->Void{
         setAudioProperties()
